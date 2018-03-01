4a98439b999196738609d54abdf98973ab6d11d1
Maxim <maxim.duzij@hotmail.com>
2018-03-01 18:38:19 +0100

    Buisness layer project added

---
+++ Rigatni.CMS/Riganti.CMS.BL/Installers/DataAccessInstaller.cs
@@ -0,0 +1,42 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Microsoft.EntityFrameworkCore;
+using Riganti.Utils.Infrastructure.AutoMapper;
+using Riganti.Utils.Infrastructure.Core;
+using Riganti.Utils.Infrastructure.EntityFrameworkCore;
+using Riganti.Utils.Infrastructure.Services.Facades;
+using Rigatni.CMS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riganti.CMS.BL.Installers
+{
+    public class DataAccessInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+
+                  Component.For<Func<DbContext>>()
+                    .Instance(() => new CmsDbContext())
+                    .LifestyleSingleton(),
+
+                Component.For<IUnitOfWorkProvider>()
+                    .ImplementedBy<EntityFrameworkUnitOfWorkProvider>()
+                    .LifestyleSingleton(),
+
+                Component.For(typeof(IRepository<,>))
+                    .ImplementedBy(typeof(EntityFrameworkRepository<,>))
+                    .IsFallback()
+                    .LifestyleTransient(),
+
+                Component.For(typeof(IEntityDTOMapper<,>))
+                    .ImplementedBy(typeof(EntityDTOMapper<,>))
+                    .LifestyleSingleton()
+
+                );
+        }
+    }
+}
